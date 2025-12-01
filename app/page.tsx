26df44a9e53ed80c6a201d5e097a92baa2e0380c
jerrykizz <ja.ash9@outlook.com>
2025-12-01 13:25:57 -0500

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+export default function Page() {
+  return (
+    <main style={{ padding: 40 }}>
+      <h1 style={{ fontSize: 40, fontWeight: "bold" }}>
+        Afrikanhauz Marketplace
+      </h1>
+      <p style={{ marginTop: 20 }}>
+        Post jobs. Sell products. Hire professionals. Buy goods.
+      </p>
+    </main>
+  );
+}
